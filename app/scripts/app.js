399966f282a9bc3226bf278494176fd4316dec8a
Katrina Schreiber <schreiber.katrina@gmail.com>
2016-06-18 18:26:51 -0500

    rearranging pages and setting each page appropriately to flow properly

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,6 +28,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/listCreator', {
+        templateUrl: 'views/listCreator.html',
+        controller: 'listCreatorCtrl'
+      })
       .when('/errandMapper', {
         templateUrl: 'views/errandMapper.html',
         controller: 'errandMapperCtrl'
---
+++ app/scripts/controllers/listCreator.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('listCreatorCtrl', function ($scope) {
+    $scope.todos = [];
+    $scope.addTodo = function () {          
+      $scope.todos.push($scope.todo);         // pushes new todo items onto existing todo items array
+      $scope.todo = '';                       // then clears the text input field
+    };
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);          // functino that uses params splice(index aka where to add/remove, remove this many items, new items to be added to array)
+    };                                        // so this is saying at the index of the ng-repeat, remove one item from the array.
+  });
